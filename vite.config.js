c6502116af6f506303daa423479713557c57aa1c
techi5ive12345-solutions <techi5ive12345@gmail.com>
2026-06-08 13:44:25 +0500

    api changed

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://frontend-puce-seven-74.vercel.app/',
         changeOrigin: true,
       },
     },
